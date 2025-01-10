9b2d81b0360dd0dcd77294979ea4f6f593f45b05
ragnarrlaw <carmichealclair@yahoo.com>
2025-01-11 00:05:23 +0530

    add parser - basic set up

---
+++ src/parser/parser.c
@@ -0,0 +1,4 @@
+#include "parser.h"
+#include <stdlib.h>
+
+struct parser *parser_init() { return NULL; }
---
+++ src/parser/parser.h
@@ -0,0 +1,8 @@
+#ifndef PARSER_H
+#define PARSER_H
+
+struct parser {};
+
+struct parser *parser_init();
+
+#endif // PARSER_H
