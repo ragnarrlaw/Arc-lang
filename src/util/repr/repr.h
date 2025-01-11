c9afbb89f365e1852db02de16971a042da4594bf
ragnarrlaw <carmichealclair@yahoo.com>
2025-01-11 11:41:41 +0530

    define repr struct field representation marco

---
+++ src/util/repr/repr.h
@@ -0,0 +1,15 @@
+#ifndef REPR_H
+#define REPR_H
+
+#define PRINT_FIELD_INT(structure, field) printf(#field ": %d\n", structure->field)
+#define PRINT_FIELD_STR(structure, field) printf(#field ": %s\n", structure->field)
+#define PRINT_FIELD_CHAR_ARRAY(structure, field, len) \
+  do { \
+    printf(#field ": "); \
+    for (size_t i = 0; i < len; i++) { \
+      printf("%c", structure->field[i]); \
+    } \
+    printf("\n"); \
+  } while (0)
+
+#endif // !REPR_H
