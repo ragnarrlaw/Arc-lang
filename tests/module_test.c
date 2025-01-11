7b75cf3963c4c44c9d2d4530cc12b120ba05e633
ragnarrlaw <carmichealclair@yahoo.com>
2025-01-11 11:30:37 +0530

    update tests to use the test utils

--- tests/module_test.c
+++ tests/module_test.c
@@ -1,18 +1,23 @@
-#include "../tests/lexer/lexer_test.h"
+#include "lexer/lexer_test.h"
+#include "utils/util.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 int main() {
   printf("Running tests...\n");
 #ifdef MODULE_LEXER_TEST
-  test_single_line_function();
-  test_multi_line_function();
-  test_multi_line_function_with_braces();
-  test_single_line_let();
-  test_multi_line_let();
-  test_punctuation();
-  test_program();
+  RUN_TEST(test_single_line_function);
+  RUN_TEST(test_multi_line_function);
+  RUN_TEST(test_multi_line_function_with_braces);
+  RUN_TEST(test_single_line_let);
+  RUN_TEST(test_multi_line_let);
+  RUN_TEST(test_punctuation);
+  RUN_TEST(test_single_line_comment);
+  RUN_TEST(test_underscore_separated_identifiers);
+  RUN_TEST(test_function_call);
+  RUN_TEST(test_program);
 #endif
+  PRINT_TEST_RESULTS();
   printf("Done.\n");
   return EXIT_SUCCESS;
 }
