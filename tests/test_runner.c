125cb8bae8678f04b808dbcd864bd85504b70488
ragnarrlaw <carmichealclair@yahoo.com>
2025-01-19 19:15:28 +0530

    update test runner to include parser tests and improve output messages - incomplete need to add parser tests for other expressions and literals

--- tests/test_runner.c
+++ tests/test_runner.c
@@ -1,12 +1,17 @@
 #include "lexer_test.h"
+#include "parser_test.h"
 #include "test_util.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 int main() {
-  printf("Running tests...\n");
+  printf("Running lexer tests...\n");
   lexer_run_all_tests();
-  PRINT_TEST_RESULTS();
   printf("Done.\n");
+
+  printf("Running parser tests...\n");
+  parser_run_all_tests();
+  printf("Done.\n");
+
   return EXIT_SUCCESS;
 }
