cab731472d129d0396c8b7231f9bd94f132990c2
ragnarrlaw <carmichealclair@yahoo.com>
2025-01-21 18:39:44 +0530

    refactor print macros for improved readability and consistency

--- include/util_repr.h
+++ include/util_repr.h
@@ -1,23 +1,25 @@
 #ifndef REPR_H
 #define REPR_H
 
-#define PRINT_FIELD_INT(structure, field) printf(#field ": %d\n", structure->field)
-#define PRINT_FIELD_STR(structure, field) printf(#field ": %s\n", structure->field)
-#define PRINT_FIELD_CHAR_ARRAY(structure, field, len) \
-  do { \
-    printf(#field ": "); \
-    for (size_t i = 0; i < len; i++) { \
-      printf("%c", structure->field[i]); \
-    } \
-    printf("\n"); \
+#define PRINT_FIELD_INT(structure, field)                                      \
+  printf(#field ": %d\n", structure->field)
+#define PRINT_FIELD_STR(structure, field)                                      \
+  printf(#field ": %s\n", structure->field)
+#define PRINT_FIELD_CHAR_ARRAY(structure, field, len)                          \
+  do {                                                                         \
+    printf(#field ": ");                                                       \
+    for (size_t i = 0; i < len; i++) {                                         \
+      printf("%c", structure->field[i]);                                       \
+    }                                                                          \
+    printf("\n");                                                              \
   } while (0)
 
-#define PRINT_CHAR_ARRAY(arr, len) \
-  do { \
-    for (size_t i = 0; i < len; i++) { \
-      printf("%c", arr[i]); \
-    } \
-    printf("\n"); \
+#define PRINT_CHAR_ARRAY(arr, len)                                             \
+  do {                                                                         \
+    for (size_t i = 0; i < len; i++) {                                         \
+      printf("%c", arr[i]);                                                    \
+    }                                                                          \
+    printf("\n");                                                              \
   } while (0)
 
 #endif // !REPR_H
