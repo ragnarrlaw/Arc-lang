f54d61f349a3c022a7b9c658e2e52f9e90595e97
ragnarrlaw <carmichealclair@yahoo.com>
2025-01-30 23:59:31 +0530

    feat: reorganize kv.h for improved readability and maintainability

--- include/kv.h
+++ include/kv.h
@@ -2,7 +2,10 @@
 #ifndef KV_H
 #define KV_H
 
+// clang-format off
+
 #include "object_t.h"
+#include "util_error.h"
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,29 +13,25 @@
 #define HT_INITIAL_CAPACITY 16
 #define HT_LOAD_FACTOR 0.75
 
-// clang-format off
-
-// hash table entry (key-value pair)
 typedef struct entry {
-  char *key;
-  struct obj_t *value;
-  struct entry *next; // for collisions
+    char *key;
+    struct obj_t *value;
+    struct entry *next;
 } entry;
 
-// hash table structure
 typedef struct hash_table {
-  entry **buckets; // array of pointers to entries
-  size_t size;     // number of entries
-  size_t capacity; // number of buckets
+    entry **buckets;
+    size_t size;
+    size_t capacity;
 } hash_table;
 
-// iterator for GC marking
 typedef struct hash_table_iterator {
-  hash_table *table;
-  size_t bucket_index;
-  entry *current_entry;
+    hash_table *table;
+    size_t bucket_index;
+    entry *current_entry;
 } hash_table_iterator;
 
+// hash table interface
 hash_table *hash_table_init();
 void hash_table_free(hash_table *table);
 void hash_table_insert(hash_table *table, const char *key, struct obj_t *value);
@@ -40,8 +39,10 @@ struct obj_t *hash_table_get(hash_table *table, const char *key);
 bool hash_table_remove(hash_table *table, const char *key);
 bool hash_table_has(hash_table *table, const char *key);
 
+// iterator interface
 hash_table_iterator hash_table_iterate(hash_table *table);
 bool hash_table_next(hash_table_iterator *it, const char **key, struct obj_t **value);
+
 
 #endif // !KV_H
 
