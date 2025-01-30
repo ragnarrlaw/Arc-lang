12ca5862dd0b4a418410a379b655349d04c41fc2
ragnarrlaw <carmichealclair@yahoo.com>
2025-01-30 23:58:32 +0530

    feat: update evaluation functions to accept environment parameter for improved context handling

--- include/evaluator.h
+++ include/evaluator.h
@@ -2,10 +2,11 @@
 #define EVALUATOR_H
 
 #include "ast.h"
+#include "environment.h"
 #include "object_t.h"
 
-struct obj_t *evaluate_program(struct program *);
-struct obj_t *evaluate_statement(struct statement *);
-struct obj_t *evaluate_expression(struct expression *);
+struct obj_t *evaluate_program(struct environment *env, struct program *);
+struct obj_t *evaluate_statement(struct environment *env, struct statement *);
+struct obj_t *evaluate_expression(struct environment *env, struct expression *);
 
 #endif // !EVALUATOR_H
