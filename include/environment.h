55d7a0d391dc8783d9d53b86284ac143dfec3d99
ragnarrlaw <carmichealclair@yahoo.com>
2025-01-30 18:24:18 +0530

    feat: implement environment structure for variable management and scoping

---
+++ include/environment.h
@@ -0,0 +1,50 @@
+#ifndef ENVIRONMENT_H
+#define ENVIRONMENT_H
+
+/**
+ * environment tracks active variables/objects
+ */
+
+#include "kv.h"
+#include "object_t.h"
+#include <stddef.h>
+
+#define MAX_ROOTS 1024
+
+typedef struct environment environment;
+
+struct environment {
+  struct environment *parent;
+  struct hash_table *symbols; // k-v store for storing variables and data
+};
+
+/**
+ * initialize an environment
+ */
+environment *env_init();
+
+/**
+ * define a variable in the environment
+ */
+void env_define(environment *env, const char *name, struct obj_t *value);
+
+/**
+ * search the environment and it's parents for a key, if found
+ * the value in the key is overriden, else a new key entry is
+ * added to the current environment
+ */
+void env_set(environment *env, const char *name, struct obj_t *value);
+
+/**
+ * look up a variable name or a value in the current environment
+ * if not found in the current environment, go up the parent heirarchy
+ * looking for the key, if not found returns NULL.
+ */
+struct obj_t *env_look_up(environment *env, char *key);
+
+/**
+ * free the environment, release the resources
+ */
+void env_free(environment *env);
+
+#endif // !ENVIRONMENT_H
---
+++ src/environment.c
@@ -0,0 +1,53 @@
+#include "environment.h"
+#include "kv.h"
+#include "util_error.h"
+#include <stdlib.h>
+
+environment *env_init() {
+  environment *env = malloc(sizeof(environment));
+  if (!env) {
+    return NULL;
+  }
+  struct hash_table *table = hash_table_init();
+  if (!table) {
+    ERROR_LOG("error while allocating memory\n");
+    free(env);
+    return NULL;
+  }
+  env->parent = NULL;
+  env->symbols = table;
+  return env;
+}
+
+void env_free(environment *env) {
+  if (env) {
+    hash_table_free(env->symbols);
+    free(env);
+  }
+  env = NULL;
+}
+
+void env_define(environment *env, const char *name, struct obj_t *value) {
+  hash_table_insert(env->symbols, name, value);
+}
+
+void env_set(environment *env, const char *name, struct obj_t *value) {
+  environment *current = env;
+  while (current) {
+    if (hash_table_has(current->symbols, name)) {
+      hash_table_insert(current->symbols, name, value);
+      return;
+    }
+    current = current->parent;
+  }
+  // if nothing is found then define the key in the current env
+  env_define(env, name, value);
+}
+
+struct obj_t *env_look_up(environment *env, char *key) {
+  struct obj_t *value = hash_table_get(env->symbols, key);
+  if (!value && env->parent) {
+    return env_look_up(env->parent, key);
+  }
+  return value;
+}
