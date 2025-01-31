8a53f5b0316d4b6991560a2dd3a5232a7fb1dadf
ragnarrlaw <carmichealclair@yahoo.com>
2025-01-31 16:34:18 +0530

    feat: modify hash table interface to use generic pointer types for values

--- include/kv.h
+++ include/kv.h
@@ -4,7 +4,6 @@
 
 // clang-format off
 
-#include "object_t.h"
 #include "util_error.h"
 #include <stdbool.h>
 #include <stdlib.h>
@@ -34,14 +33,14 @@ typedef struct hash_table_iterator {
 // hash table interface
 hash_table *hash_table_init();
 void hash_table_free(hash_table *table);
-void hash_table_insert(hash_table *table, const char *key, struct obj_t *value);
+void hash_table_insert(hash_table *table, const char *key, void *value);
 struct obj_t *hash_table_get(hash_table *table, const char *key);
 bool hash_table_remove(hash_table *table, const char *key);
 bool hash_table_has(hash_table *table, const char *key);
 
 // iterator interface
 hash_table_iterator hash_table_iterate(hash_table *table);
-bool hash_table_next(hash_table_iterator *it, const char **key, struct obj_t **value);
+bool hash_table_next(hash_table_iterator *it, const char **key, void **value);
 
 
 #endif // !KV_H
